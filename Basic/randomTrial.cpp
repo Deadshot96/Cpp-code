d86cbd7bc91ba3757ba7773e478df424e1a86eb5
Bhushan <bhushan1810patil@gmail.com>
2020-10-26 00:19:00 +0530

    add seeding using time

--- Basic/randomTrial.cpp
+++ Basic/randomTrial.cpp
@@ -1,12 +1,14 @@
 # include <iostream>
 # include <stdlib.h>
+# include <time.h>
 using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
     /* code */
-    srand(20);
+    srand(time(0));
+    cout << time(0) << endl;
 
     for (short i = 0; i < 10; i++)
     {
