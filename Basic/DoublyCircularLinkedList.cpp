5f0feb6f50f31ac26044e59c57eb7960636997eb
Bhushan <bhushan1810patil@gmail.com>
2021-01-09 16:41:13 +0530

    added Node class to DoublyCircularLinkedList

--- Basic/DoublyCircularLinkedList.cpp
+++ Basic/DoublyCircularLinkedList.cpp
@@ -1,6 +1,30 @@
 # include <iostream>
 using namespace std;
 
+class Node{
+    public:
+        int key, data;
+        Node *prev, *next;
+
+        Node(){
+            key = 0;
+            data = 0;
+            prev = nullptr;
+            next = nullptr;
+        }
+
+        Node(int key, int data = 0, Node* prev = nullptr, Node* next = nullptr){
+            this->key = key;
+            this->data = data;
+            this->prev = prev;
+            this->next = next;
+        }
+
+        void show(){
+            cout << "Key: " << key << "\tData: " << data << endl;
+        }
+};
+
 int main()
 {
     cout << "Hello, World!" << endl;
