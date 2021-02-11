cf429a31e2ef004424c8dad39683972ebaebce7a
Bhushan <bhushan1810patil@gmail.com>
2021-02-11 13:01:00 +0530

    Scalaton of BinarySeachTree Class

--- DSA/BinarySearchTree.cpp
+++ DSA/BinarySearchTree.cpp
@@ -9,7 +9,7 @@ class BinarySearchTree{
     public:
         BinarySearchTree(); // Zero Constructor
         BinarySearchTree(const BinarySearchTree & rhs); // copy Constructor
-        BinarySearchTree(const BinarySearchTree && rhs) // move Constructor
+        BinarySearchTree(const BinarySearchTree && rhs); // move Constructor
         ~BinarySearchTree(); // Destructor
 
         const Comparable & findMax() const;
@@ -19,7 +19,7 @@ class BinarySearchTree{
         void printTree(ostream & out = cout) const;
 
         void makeEmpty();
-        void insert(const Comparator & x); // lvalue insert
+        void insert(const Comparable & x); // lvalue insert
         void insert(Comparable && x); // rvalue insert
         void remove(const Comparable & x);
 
@@ -38,11 +38,25 @@ class BinarySearchTree{
             BinaryNode(const Comparable && theElement, BinaryNode *lt = nullptr, BinaryNode *rt = nullptr)
                 : element{move(theElement)}, left{lt}, right{rt} {}
 
-        }
+        };
 
         BinaryNode *root;
 
+        void insert(const Comparable & x, BinaryNode * & t);
+        void insert(Comparable && x, BinaryNode * & t);
+        void remove(const Comparable & x, BinaryNode * & t);
+        BinaryNode * findMax(BinaryNode * t) const;
+        BinaryNode * findMin(BinaryNode * t) const;
+        bool contains (const Comparable & x, BinaryNode *t) const;
+        void makeEmpty(BinaryNode * & t);
+        void printTree(BinaryNode * t, ostream & out = cout) const;
+        BinaryNode * clone (BinaryNode * t) const;
+
+};
 
 
-
+int main()
+{
+    cout << "Hello, World!\n";
+    return 0;
 }
