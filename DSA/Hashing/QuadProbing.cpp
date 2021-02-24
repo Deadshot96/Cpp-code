33545af8a9a16c0e5c42afdf6cd7459216d602ca
Bhushan <bhushan1810patil@gmail.com>
2021-02-24 19:59:59 +0530

    added HashTable class implementatioin

--- DSA/Hashing/QuadProbing.cpp
+++ DSA/Hashing/QuadProbing.cpp
@@ -5,7 +5,40 @@ using namespace std;
 template <typename HashedObj>
 class HashTable{
 
+    public:
+        explicit HashTable(int size = 101);
 
+        bool contains(const HashedObj &) const;
+        void makeEmpty();
+        bool insert(const HashedObj &);
+        bool insert(HashedObj &&);
+        bool remove(const HashedObj &);
+
+        enum EntryType {ACTIVE, EMPTY, DELETED};
+
+    private:
+
+        struct HashedEntry
+        {
+            HashedObj element;
+            EntryType info;
+
+            HashedEntry(const HashedObj & x = HashedObj{}, EntryType i = EMPTY)
+                :element{x}, info{i} {};
+
+            HashedEntry(HashedObj && x, EntryType i = EMPTY)
+                :element{move(x)}, info{i} {};
+
+        };
+
+        vector<HashedEntry> array;
+        int currentSize;
+
+        bool isActive(int) const;
+        int findPos(const HashedObj &) const;
+        void rehash();
+        size_t myhash(const HashedObj &) const;
+        
 };
 
 int main()
