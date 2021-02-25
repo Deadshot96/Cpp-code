01866aa0f04fbebaaa9ea5a12343c5f63fa020e7
Bhushan <bhushan1810patil@gmail.com>
2021-02-26 00:18:16 +0530

    added BinaryHeap class implementation

--- DSA/Heaps/BinaryHeap.cpp
+++ DSA/Heaps/BinaryHeap.cpp
@@ -4,7 +4,27 @@ using namespace std;
 
 template <typename Comparable>
 class BinaryHeap{
-    
+    public:
+        explicit BinaryHeap(int capacity = 100);
+        explicit BinaryHeap(const vector<Comparable> & items);
+
+        bool isEmpty() const;
+        const Comparable & findMin() const;
+
+        void insert(const Comparable &);
+        void insert(Comparable &&);
+        void deleteMin();
+        void deleteMin(const Comparable &);
+        void makeEmpty();
+
+    private:
+        int currentSize;
+        vector<Comparable> array;
+
+        void buildHeap();
+        void percolateDown(int);
+        void percolateUp(int);
+
 };
 
 int main()
