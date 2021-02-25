4d03dde31b1917cbb606a86499b3ea1c044c9c76
Bhushan <bhushan1810patil@gmail.com>
2021-02-25 22:23:39 +0530

    added isPrime and nextPrime definitions

--- DSA/Hashing/CuckooHashTable.cpp
+++ DSA/Hashing/CuckooHashTable.cpp
@@ -55,6 +55,39 @@ class CuckooHashTable{
 
 };
 
+
+bool isPrime(int n){
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n%2 == 0 || n%3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n%i == 0 || n%(i + 2) == 0)
+            return false;
+
+    return true; 
+}
+
+int nextPrime(int n){
+    if (n <= 1)
+        return 2;
+    
+    int prime = n;
+    bool found = false;
+
+    while (!found){
+        prime++;
+
+        if (isPrime(prime))
+            found = true;
+    }
+
+    return prime;
+}
+
+
 int main(int argc, char const *argv[])
 {
     cout << "Hellow, World!" << endl;
