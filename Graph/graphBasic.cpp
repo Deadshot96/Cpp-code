eb9e2b7b866b43d3c9969732886f4024969bfb1e
Bhushan <bhushan1810patil@gmail.com>
2021-03-18 00:10:22 +0530

    added BFS to graph

--- Graph/graphBasic.cpp
+++ Graph/graphBasic.cpp
@@ -24,7 +24,31 @@ void Graph :: addEdge(int u, int v){
 }
 
 void Graph :: BFS(int s){
+    bool *visited = new bool[V];
 
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    list<int> queue;
+
+    visited[s] = true;
+    queue.push_back(s);
+
+    list<int>::iterator i;
+
+    while (!queue.empty()){
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+
+
+        for (i = adj[s].begin(); i != adj[s].end(); i++){
+            if (!visited[*i]){
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
 }
 
 
