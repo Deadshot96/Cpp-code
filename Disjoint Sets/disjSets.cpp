611c3cb0f34419379ac10ffd6f6eada9b93db76a
Bhushan <bhushan1810patil@gmail.com>
2021-03-17 14:32:00 +0530

    added methods of find and unionSets to DisjSets

--- Disjoint Sets/disjSets.cpp
+++ Disjoint Sets/disjSets.cpp
@@ -1,5 +1,46 @@
 # include <iostream>
+# include <vector>
 using namespace std;
+
+class DisjSets{
+    public:
+        explicit DisjSets(int);
+        int find(int) const;
+        int find(int);
+        void unionSets(int, int);
+
+    private:
+        vector<int> s;
+};
+
+DisjSets :: DisjSets(int numElements) 
+    : s{numElements, -1} {}
+
+
+/**
+ * Union two disjoint sets.
+ * For simplicity, we assume root1 and root2 are distinct
+ * and represent set names.
+ * root1 is the root of set 1.
+ * root2 is the root of set 2.
+*/
+void DisjSets :: unionSets(int root1, int root2){
+    s[root2] = root1;
+}
+
+
+/**
+ * Perform a find.
+ * Error checks omitted again for simplicity.
+ * Return the set containing x.
+*/
+
+int DisjSets :: find(int x) const{
+    if (s[x] < 0)
+        return x;
+    else
+        return find(s[x]);
+}
 
 int main()
 {
