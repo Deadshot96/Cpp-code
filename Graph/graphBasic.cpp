45bc51c311604632deb5275ef950edc575ba1a8a
Bhushan <bhushan1810patil@gmail.com>
2021-03-17 23:59:46 +0530

    added basic graph class implemetation

--- Graph/graphBasic.cpp
+++ Graph/graphBasic.cpp
@@ -1,10 +1,19 @@
 # include <iostream>
 # include <vector>
+# include <list>
 using namespace std;
 
 class Graph{
+    private:
+        int V;
+        list<int> *adj;
 
+    public:
+        Graph(int);
+        void addEdge(int, int);
+        void BFS(int);
 };
+
 
 int main()
 {
