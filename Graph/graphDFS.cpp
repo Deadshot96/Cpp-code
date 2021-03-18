aaea0ff14bddc77c8d3af5021ad89fac6cdb88f4
Bhushan <bhushan1810patil@gmail.com>
2021-03-19 01:54:02 +0530

    added DFS implementation for a hashable template graph

--- Graph/graphDFS.cpp
+++ Graph/graphDFS.cpp
@@ -1,7 +1,9 @@
 # include <iostream>
-# include <vector>
+# include <map>
 # include <list>
+# include <vector>
 # include <unordered_map>
+# include <unordered_set>
 using namespace std;
 
 template <typename Hashable>
@@ -9,13 +11,16 @@ class Graph{
 
     private:
         int V;
-        unordered_map<Hashable, vector<Hashable>> adj;
+        unordered_set<Hashable> vertices;
+        unordered_map<Hashable, list<Hashable>> adj;
+
+        void DFSUtil(Hashable, unordered_map<Hashable, bool> &) const; 
 
     public:
         Graph(int);
         void addEdge(Hashable, Hashable);
-        void DFS(Hashable) const; 
         void DFS() const;
+        void DFS(Hashable) const;
 };
 
 template <typename Hashable>
@@ -26,9 +31,48 @@ Graph<Hashable> :: Graph(int V){
 
 template <typename Hashable>
 void Graph<Hashable> :: addEdge(Hashable u, Hashable v){
+    
+    vertices.insert(u);
+    vertices.insert(v);
+
     adj[u].push_back(v);
+    // adj[v].push_back(u);
 }
 
+template <typename Hashable>
+void Graph<Hashable> :: DFS() const{
+    unordered_map<Hashable, bool> visited;
+
+    for(auto& i : adj)
+        if (visited[i.first] == false)
+            DFSUtil(i.first, visited);
+
+    cout << endl;
+}
+
+template <typename Hashable>
+void Graph<Hashable> :: DFS(Hashable v) const{
+    unordered_map<Hashable, bool> visited;
+    DFSUtil(v, visited);
+    cout << endl;
+}
+
+
+template <typename Hashable>
+void Graph<Hashable> :: DFSUtil(Hashable v, unordered_map<Hashable, bool>& visited) const{
+    visited[v] = true;
+    cout << v << "\t";
+
+    if (adj.find(v) == adj.end())
+        return;
+
+    typename list<Hashable>::iterator i;
+    list<Hashable> links = adj.at(v);
+
+    for (i = links.begin(); i != links.end(); ++i)
+		if (!visited[*i])
+		    DFSUtil(*i, visited);
+}
 
 int main()
 {
@@ -48,5 +92,7 @@ int main()
     g.addEdge(5, 2);
     g.addEdge(2, 0);
 
+    g.DFS();
+    g.DFS(6);
     return 0;
 }
