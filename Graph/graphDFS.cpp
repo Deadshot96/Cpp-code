e58650f5c92185033f9978e76127faaefd15f3ff
Bhushan <bhushan1810patil@gmail.com>
2021-03-18 23:51:52 +0530

    added starter methods to graphDFS.cpp

--- Graph/graphDFS.cpp
+++ Graph/graphDFS.cpp
@@ -1,34 +1,52 @@
 # include <iostream>
 # include <vector>
 # include <list>
+# include <unordered_map>
 using namespace std;
 
-template <typename Comparable>
+template <typename Hashable>
 class Graph{
 
     private:
         int V;
-        list<Comparable> *adj;
+        unordered_map<Hashable, vector<Hashable>> adj;
 
     public:
         Graph(int);
-        addEdge(Comparable, Comparable);
-        BFS(Comparable) const; 
-        BFS() const;
+        void addEdge(Hashable, Hashable);
+        void DFS(Hashable) const; 
+        void DFS() const;
 };
 
-template <typename Comparable>
-Graph<Comparable> :: Graph(int V){
+template <typename Hashable>
+Graph<Hashable> :: Graph(int V){
     this->V = V;
-    adj = new list<Comparable> [V];
 }
 
 
-
+template <typename Hashable>
+void Graph<Hashable> :: addEdge(Hashable u, Hashable v){
+    adj[u].push_back(v);
+}
 
 
 int main()
 {
     cout << "Hello, World!" << endl;
+
+    Graph<int> g(7);
+    g.addEdge(3, 0);
+    g.addEdge(3, 1);
+    g.addEdge(3, 4);
+    g.addEdge(3, 6);
+    g.addEdge(3, 5);
+    g.addEdge(3, 2);
+    g.addEdge(0, 1);
+    g.addEdge(1, 4);
+    g.addEdge(4, 6);
+    g.addEdge(6, 5);
+    g.addEdge(5, 2);
+    g.addEdge(2, 0);
+
     return 0;
 }
