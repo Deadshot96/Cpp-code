cfbebed96e9db88666dcce4922bcb021d8539c7d
Bhushan <bhushan1810patil@gmail.com>
2021-03-19 22:32:39 +0530

    created graphMother file to code the find the mother node algo

---
+++ Graph/graphMother.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <list>
+# include <unordered_map>
+# include <unordered_set>
+
+/*
+    A mother vertex in a graph G = (V,E) is a vertex v 
+    such that all other vertices in G can be reached by 
+    a path from v.
+*/
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
