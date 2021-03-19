2254f866d967395b2beedace57504237fc25b69b
Bhushan <bhushan1810patil@gmail.com>
2021-03-19 21:37:44 +0530

    added BFS to graph class at graphBFS.cpp

--- Graph/graphBFS.cpp
+++ Graph/graphBFS.cpp
@@ -1,7 +1,9 @@
 # include <iostream>
 # include <list>
 # include <unordered_map>
+# include <queue>
 # include <unordered_set>
+// # include <bits/stdc++.h>
 using namespace std;
 
 template <typename Hashable>
@@ -11,7 +13,7 @@ class Graph{
         unordered_set<Hashable> vertices;
         unordered_map<Hashable, list<Hashable>> adjList;
 
-        void BFSUtil(const Hashable &, unordered_map<Hashable, bool>);
+        void BFSUtil(const Hashable &, unordered_map<Hashable, bool> &) const;
 
     public:
         Graph(int);
@@ -34,6 +36,50 @@ void Graph<Hashable> :: addEdge(Hashable u, Hashable v){
     adjList[v].push_back(u);
 }
 
+template <typename Hashable>
+void Graph<Hashable> :: BFS() const{
+    unordered_map<Hashable, bool> visited;
+    cout << "Breath First Search: \n";
+
+    for(auto& i: adjList)
+        if (!visited[i.first])
+            BFSUtil(i.first, visited);
+
+    cout << endl;
+}
+
+template <typename Hashable>
+void Graph<Hashable> :: BFS(const Hashable & v) const{
+    cout << "Breadth First Search: \n";
+    unordered_map<Hashable, bool> visited;
+    BFSUtil(v, visited);
+    cout << endl;
+}
+
+template <typename Hashable>
+void Graph<Hashable> :: BFSUtil(const Hashable & v, unordered_map<Hashable, bool> & visited) const{
+    visited[v] = true;
+
+    list<Hashable> queue;
+    queue.push_back(v);
+
+    while(!queue.empty()){
+        Hashable x = queue.front();
+        cout << x << "\t";
+        queue.pop_front();
+
+        list<Hashable> links = adjList.at(x);
+        typename list<Hashable>::iterator itr;
+
+        for (itr = links.begin(); itr != links.end(); ++itr){
+            if (!visited[*itr]){
+                visited[*itr] = true;
+                queue.push_front(*itr);
+            }
+        }
+    }
+}
+
 int main()
 {
     cout << "Hello, World!" << endl;
@@ -52,6 +98,9 @@ int main()
     g.addEdge(6, 5);
     g.addEdge(5, 2);
     g.addEdge(2, 0);
+
+    g.BFS();
+    g.BFS(3);
 
     return 0;
 }
