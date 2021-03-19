80097052c26fd8214d70c28708aeb8bf6e0e3e52
Bhushan <bhushan1810patil@gmail.com>
2021-03-19 12:41:55 +0530

    added Graph class structure

--- Graph/graphBFS.cpp
+++ Graph/graphBFS.cpp
@@ -6,7 +6,18 @@ using namespace std;
 
 template <typename Hashable>
 class Graph{
+    private:
+        int numVertices;
+        unordered_set<Hashable> vertices;
+        unordered_map<Hashable, list<Hashable>> adjList;
 
+        void BFSUtil(const Hashable &, unordered_map<Hashable, bool>);
+
+    public:
+        Graph(int);
+        void addEdge(Hashable, Hashable);
+        void BFS() const;
+        void BFS(const Hashable &) const;
 };
 
 int main()
