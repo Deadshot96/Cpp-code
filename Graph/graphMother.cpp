8b383db24850abdf9cbb6d03ac522b99a2f1bee5
Bhushan <bhushan1810patil@gmail.com>
2021-03-20 15:50:48 +0530

    added Graph and addEdge to graphMother.cpp

--- Graph/graphMother.cpp
+++ Graph/graphMother.cpp
@@ -26,8 +26,36 @@ class Graph{
         void DFS(Hashable) const;
 };
 
+template <typename Hashable>
+Graph<Hashable> :: Graph(int numVertices){
+    this->numVertices = numVertices;
+}
+
+template <typename Hashable>
+void Graph<Hashable> :: addEdge(Hashable u, Hashable v){
+    vertices.insert(u);
+    vertices.insert(v);
+
+    adjList[u].push_back(v);
+}
+
 int main()
 {
     cout << "Hello, World!" << endl;
+    Graph <int> g(7);
+    g.addEdge(2, 0);
+    g.addEdge(2, 5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 3);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(4, 6);
+    g.addEdge(6, 5);
+    g.addEdge(3, 2);
+    g.addEdge(3, 5);
+    g.addEdge(3, 6);
+    g.addEdge(3, 4);
+
+
     return 0;
 }
