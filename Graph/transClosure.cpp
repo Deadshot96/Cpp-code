a53ff71b76e929272215f308370e41a6c07da9aa
Bhushan <bhushan1810patil@gmail.com>
2021-03-20 20:05:11 +0530

    added constructor to transClosure.cpp

--- Graph/transClosure.cpp
+++ Graph/transClosure.cpp
@@ -11,7 +11,7 @@ class Graph{
         int numVertices;
         unordered_set<Hashable> vertices;
         unordered_map<Hashable, list<Hashable>> adjList;
-        vector<vector<Hashable>> transClosure;
+        vector<vector<Hashable>> transClosureMat;
         bool transClosureFlag = false;
 
         void DFSUtil(const Hashable &, unordered_map<Hashable, bool> &);
@@ -23,6 +23,12 @@ class Graph{
         void DFS(const Hashable &) const;
         void printTransClosure() const;
 };
+
+template <typename Hashable>
+Graph<Hashable> :: Graph(int numVertices){
+    this->numVertices = numVertices;
+    vector<vector<bool>> transClosureMat(numVertices, vector<bool>(numVertices, false));
+}
 
 int main()
 {
