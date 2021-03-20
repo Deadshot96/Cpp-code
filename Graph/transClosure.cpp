86d75e173d97e4462ca5449f6a5dc911c35dc175
Bhushan <bhushan1810patil@gmail.com>
2021-03-20 19:49:59 +0530

    created and added transClosure.cpp

---
+++ Graph/transClosure.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <vector>
+# include <unordered_map>
+# include <unordered_set>
+# include <list>
+using namespace std;
+
+class Graph{
+
+};
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
