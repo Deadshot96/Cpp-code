78541225d716c304315eec051a70e7484f1784a6
Bhushan <bhushan1810patil@gmail.com>
2021-03-20 16:09:57 +0530

    add dfs search to graphMother.cpp

--- Graph/graphMother.cpp
+++ Graph/graphMother.cpp
@@ -17,13 +17,13 @@ class Graph{
         unordered_set<Hashable> vertices;
         unordered_map<Hashable, list<Hashable>> adjList;
 
-        void DFSUtil(const Hashable &, unordered_map<Hashable, bool>) const;
+        void DFSUtil(const Hashable &, unordered_map<Hashable, bool> &) const;
 
     public:
         Graph(int);
         void addEdge(Hashable, Hashable);
         void DFS() const;
-        void DFS(Hashable) const;
+        void DFS(const Hashable &) const;
 };
 
 template <typename Hashable>
@@ -38,6 +38,43 @@ void Graph<Hashable> :: addEdge(Hashable u, Hashable v){
 
     adjList[u].push_back(v);
 }
+
+template <typename Hashable>
+void Graph<Hashable> :: DFS() const{
+    unordered_map<Hashable, bool> visited;
+    cout << "Depth First Search: \n";
+    
+    for (auto item: adjList)
+        if (!visited[item.first]){
+            DFSUtil(item.first, visited);
+            cout << endl;
+        }
+}
+
+template <typename Hashable>
+void Graph<Hashable> :: DFS(const Hashable & v) const{
+    cout << "Depth First Search: \n";
+    unordered_map<Hashable, bool> visited;
+    DFSUtil(v, visited);
+    cout << endl;
+}
+
+template <typename Hashable>
+void Graph<Hashable> :: DFSUtil(const Hashable & v, unordered_map<Hashable, bool> & visited) const{
+    visited[v] = true;
+    cout << v << "\t";
+
+    if (adjList.find(v) == adjList.end())
+        return;
+
+    const list<Hashable> & links = adjList.at(v);
+    typename list<Hashable>::const_iterator itr;
+
+    for (itr = links.begin(); itr != links.end(); ++itr)
+        if (!visited[*itr])
+            DFSUtil(*itr, visited);
+}
+
 
 int main()
 {
@@ -56,6 +93,8 @@ int main()
     g.addEdge(3, 6);
     g.addEdge(3, 4);
 
+    g.DFS();
+    g.DFS(4);
 
     return 0;
 }
