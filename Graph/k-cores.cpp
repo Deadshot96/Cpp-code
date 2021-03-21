f5b7a55774f62cc166a28d612dd567dc356c00cf
Bhushan <bhushan1810patil@gmail.com>
2021-03-21 19:02:11 +0530

    added graph

--- Graph/k-cores.cpp
+++ Graph/k-cores.cpp
@@ -33,17 +33,52 @@ Graph<Hashable> :: Graph(size_t V)
     : V(V) {}
 
 template <typename Hashable>
-void Graph<Hashable> :: addList(const Hashable &u, const Hashable &v){
+void Graph<Hashable> :: addEdge(const Hashable &u, const Hashable &v){
     vertices.insert(u);
     vertices.insert(v);
 
-    addList[u].push_back(v);
-    addList[v].push_back(u);
+    adjList[u].push_back(v);
 }
 
 
 int main(int argc, char const *argv[])
 {
     cout << "Hello, World!\n";
+    
+    Graph<int> g(9);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 0);
+    g.addEdge(1, 2);
+    g.addEdge(1, 5);
+    g.addEdge(5, 1);
+    g.addEdge(5, 2);
+    g.addEdge(5, 6);
+    g.addEdge(5, 8);
+    g.addEdge(8, 5);
+    g.addEdge(8, 6);
+    g.addEdge(2, 0);
+    g.addEdge(2, 1);
+    g.addEdge(2, 5);
+    g.addEdge(2, 4);
+    g.addEdge(2, 3);
+    g.addEdge(2, 6);
+    g.addEdge(6, 2);
+    g.addEdge(6, 5);
+    g.addEdge(6, 8);
+    g.addEdge(6, 4);
+    g.addEdge(6, 3);
+    g.addEdge(4, 2);
+    g.addEdge(4, 6);
+    g.addEdge(4, 3);
+    g.addEdge(4, 7);
+    g.addEdge(3, 4);
+    g.addEdge(3, 2);
+    g.addEdge(3, 6);
+    g.addEdge(3, 7);
+    g.addEdge(7, 4);
+    g.addEdge(7, 3);
+    g.addEdge(7, 6);
+
     return 0;
 }
