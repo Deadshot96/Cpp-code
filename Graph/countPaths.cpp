1ca4e2be3f3157aac2865d8c08d22b5d8de5a43a
Bhushan <bhushan1810patil@gmail.com>
2021-03-23 00:24:10 +0530

    added class boilerplate and constructor in countPaths.cpp

--- Graph/countPaths.cpp
+++ Graph/countPaths.cpp
@@ -6,8 +6,20 @@ using namespace std;
 
 template <typename Hashable>
 class Graph{
+    private:
+        size_t numV;
+        unordered_map<Hashable, list<Hashable>> adjList;
+        unordered_set<Hashable> vertices;
 
+    public:
+        Graph(size_t);
+        addEdge(const Hashable &, const Hashable &);
+        void countPaths(const Hashable &, const Hashable &);
 };
+
+template <typename Hashable>
+Graph<Hashable> :: Graph(size_t numV)
+    :numV(numV) {}
 
 int main()
 {
