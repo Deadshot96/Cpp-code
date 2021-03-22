69a7bdcd0d2ed498302b18d566ca7c3ac9fa6483
Bhushan <bhushan1810patil@gmail.com>
2021-03-22 22:02:15 +0530

    added DFSUtil to iterativeDFS.cpp

--- Graph/iterativeDFS.cpp
+++ Graph/iterativeDFS.cpp
@@ -3,6 +3,7 @@
 # include <vector>
 # include <unordered_map>
 # include <unordered_set>
+# include <stack>
 using namespace std;
 
 template <typename Hashable>
@@ -11,7 +12,7 @@ class Graph{
         size_t numV;
         unordered_set<Hashable> vertices;
         unordered_map<Hashable, list<Hashable>> adjList;
-        void DFSUtil(const Hashable &, unordered_map<Hashable, bool> &);
+        void DFSUtil(const Hashable &, unordered_map<Hashable, bool> &) const;
 
     public:
         Graph(size_t);
@@ -32,10 +33,46 @@ void Graph<Hashable> :: addEdge(const Hashable &u, const Hashable &v){
     adjList[u].push_back(v);
 }
 
+template <typename Hashable>
+void Graph<Hashable> :: DFSUtil(const Hashable &u, unordered_map<Hashable, bool>& visited) const {
+    stack<Hashable> st;
+    st.push(u);
+
+    while (!st.empty()){
+        Hashable vertex = st.pop();
+
+        if (!visited[vertex]){
+            cout << vertex << "\t";
+            visited[vertex] = true;
+        }
+
+        if (adjList.find(vertex) == adjList.end())
+            continue;
+
+        const list<Hashable> & links = adjList.at(vertex);
+        typename list<Hashable> :: const_iterator itr;
+
+        for (itr = links.begin(); itr != links.end(); ++itr)
+            st.push(*itr);
+    }
+}
+
 int main()
 {
     cout << "Hello, World!\n";
 
-    Graph<int> g(8);
+    Graph<int> g(7);
+    g.addEdge(3, 0);
+    g.addEdge(3, 1);
+    g.addEdge(3, 4);
+    g.addEdge(3, 6);
+    g.addEdge(3, 5);
+    g.addEdge(3, 2);
+    g.addEdge(0, 1);
+    g.addEdge(1, 4);
+    g.addEdge(4, 6);
+    g.addEdge(6, 5);
+    g.addEdge(5, 2);
+    g.addEdge(2, 0);
     return 0;
 }
