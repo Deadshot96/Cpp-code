f7eab2201eee03fb93cd53e7b1d4eda5cb75f8cf
Bhushan <bhushan1810patil@gmail.com>
2021-03-23 19:30:00 +0530

    aded countPaths and pathCounter functions to countPaths.cpp

--- Graph/countPaths.cpp
+++ Graph/countPaths.cpp
@@ -10,6 +10,7 @@ class Graph{
         size_t numV;
         unordered_map<Hashable, list<Hashable>> adjList;
         unordered_set<Hashable> vertices;
+        void pathCounter(const Hashable &, const Hashable &, size_t &);
 
     public:
         Graph(size_t);
@@ -29,6 +30,31 @@ void Graph<Hashable> :: addEdge(const Hashable & u, const Hashable & v){
     adjList[u].push_back(v);
 }
 
+template <typename Hashable>
+void Graph<Hashable> ::countPaths(const Hashable & u, const Hashable & v){
+    size_t paths = 0;
+    pathCounter(u, v, paths);
+    cout << "Paths from " << u << " to " << v << ": " << paths << endl;
+} 
+
+template <typename Hashable>
+void Graph<Hashable> :: pathCounter(const Hashable & u, const Hashable & v, size_t & paths){
+    
+    if (u == v){
+        paths++;
+    }
+    else{
+        if (adjList.find(u) == adjList.end())
+            return;
+        
+        const list<Hashable> & links = adjList.at(u);
+        typename list<Hashable> :: const_iterator itr;
+
+        for (itr = links.begin(); itr != links.end(); ++itr)
+            pathCounter(*itr, v, paths);
+    }    
+}
+
 int main()
 {
     cout << "Hello, World!\n";
@@ -39,6 +65,9 @@ int main()
     g.addEdge('B', 'D');
     g.addEdge('B', 'E');
     g.addEdge('D', 'C');
+    g.addEdge('C', 'E');
+    
+    g.countPaths('A', 'E');
 
     return 0;
 }
